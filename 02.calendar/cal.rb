4423c373897e3edbe434ebde57a3c1dc98847176
mamiyo34 <aky.mamiyo34@gmail.com>
2024-07-31 12:50:57 +0900

    日付の幅を定数とした

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,11 +14,12 @@ last_day = Date.new(display_year, display_month, -1)
 date = Date.new(display_year, display_month, first_day.day)
 title = display_month.to_s + "月" + display_year.to_s
 day_of_week = " 日 月 火 水 木 金 土 "
+DateWidth = 3
 
-puts title.center(21) #カレンダーの幅21=1日の幅3*7日
+puts title.center(DateWidth * 7) #カレンダーの幅=1日の幅*7日
 puts day_of_week
-first_day.wday.times {print " ".to_s.rjust(3)} #月初の日の曜日をカレンダー上で合わせるための処理
+first_day.wday.times {print " ".to_s.rjust(DateWidth)} #月初の日の曜日をカレンダー上で合わせるための処理
 (first_day..last_day).each do |date|
-  print date.day.to_s.rjust(3)
+  print date.day.to_s.rjust(DateWidth)
   puts " " if date.saturday?
 end
