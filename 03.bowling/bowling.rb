d3a0174f751cfedd0bcc60639e4c7c34f2bde831
mamiyo34 <aky.mamiyo34@gmail.com>
2024-09-01 16:06:32 +0900

    bowling.rbの新規追加

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,32 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+score = ARGV[0]
+scores = score.split(',')
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
+  end
+end
+
+frames = []
+shots.each_slice(2).each_with_index do |s, i|
+  frames << [s, i]
+end
+point = 0
+
+frames.each do |frame, i|
+  if i <= 8 && frame[0] == 10 # 9フレーム目までのストライク処理
+    point += 10 + 10 + frames[i + 2][0][0].to_i if frames[i + 1][0][0].to_i == 10
+    point += 10 + frames[i + 1][0].sum unless frames[i + 1][0][0].to_i == 10
+  elsif i <= 8 && frame.sum == 10 # 9フレーム目までのスペア処理
+    point += 10 + frames[i + 1][0][0].to_i
+  else # 10フレーム目以降の処理
+    point += frame.sum
+  end
+end
+puts point
